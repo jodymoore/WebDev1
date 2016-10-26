282c68ef2f6613b3a412b7f6fdfa5b22bbdc446a
jodymoore <jodymoore@g.harvard.edu>
2016-10-26 10:45:03 -0500

    commit index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+   require("../includes/config.php");
+   include('simple_html_dom.php');
+   
+   $imageUrl = "";
+   // Create DOM from URL or file
+   $html = file_get_html('http://hometracks.nascar.com/series/KN-Pro-Series-West');
+
+   // loop thru find image src that matches $pattern 
+   foreach($html->find('img') as $element) {      
+       $subject = $element->src;
+       $href = $element->href;
+       $pattern = '/racecentral-/';
+       if (preg_match($pattern, $subject, $matches, PREG_OFFSET_CAPTURE)) {
+           $imageUrl = $subject;
+       } 
+   }
+/*
+   if($imageUrl === "") {
+       $imageUrl = "img/comingSoon.jpg";
+   }
+   else{
+       $imageUrl = "http://hometracks.nascar.com".$imageUrl;
+   }
+   
+*/
+   
+   render("colerouse.php", ['imageUrl' => $imageUrl, 'href' => $href]); 
+?>
