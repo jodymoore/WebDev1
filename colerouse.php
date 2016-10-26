a98575e21660298b3f51852aac3e5210515ba165
jodymoore <jodymoore@g.harvard.edu>
2016-10-26 10:47:28 -0500

    Add files via upload

---
+++ colerouse.php
@@ -0,0 +1,56 @@
+<body class="main-bg">
+  /* 
+  <div id="hero">  
+         
+    <a href="http://hometracks.nascar.com/nknps-west/race-central-live-douglas-county">
+      <img id="events" src="<?php echo $imageUrl; ?>"/> 
+    </a> 
+  </div>
+ */
+
+  <div id="div2">     
+  <div id="adiv">
+    <div id="pdiv">
+ <p> <font color="white"><h2>Driver No. 6 Sunrise Ford/Eibach Springs/Lucas Oil Ford</h2></font></p>
+      <p id="p1">       
+"Racing in the NASCAR K&amp;N Pro Series West is a dream come true,"Rouse, 18, said. "I'm looking forward to working with Bob (Bruncati, owner) and the Sunrise Ford Racing team this season. This year will be all about learning and improving as a driver. I believe this is a great place and a great opportunity to accomplish both of those things."</p>
+      <a href="/public/about.php"><font id="rm">Read More</font></a>
+    </div> 
+    
+  </div>
+  <div id="teasers">
+  <br/>
+        <a href="/public/gallery.php"><img id="img1" src= "/public/img/cover2.jpg"width="300px" height="200px"></a> 
+        <iframe id="iframe1" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FColeRouseRacing%2Fvideos%2F835341379916217%2F&show_text=0&width=560" width="300px" height="200px" style="border:none;overflow:hidden" scrolling="no" allowTransparency="true" allowFullScreen="true">
+        </iframe>
+    </div>
+  </div>
+  <div>
+    <div class="social-buttons">
+      <a class="btn btn-circle" href="https://www.facebook.com/ColeRouseRacing">
+        <i class="fa fa-facebook">
+          <img class="imgsc" src="/public/img/facebook.png">
+        </i>
+      </a>
+      <a class="btn btn-circle" href="https://twitter.com/colerouse2">
+        <i class="fa fa-twitter">
+         <img class="imgsc" src="/public/img/twitter.png">
+        </i>
+      </a>
+      <a class="btn btn-circle" href="https://www.instagram.com/_colerouse_/">
+        <i class="fa fa-instagram">
+         <img class="imgsc" src="/public/img/instagram.png">
+        </i>
+      </a>
+   </div>
+ </div>
+</div>	
+
+<br>
+               
+          
+             
+             
+          
+        
+ 
